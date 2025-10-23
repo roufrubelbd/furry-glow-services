a98928d4a896c5f21d11e39a2255b8433951055e
roufrubelbd <roufrubelbd@gmail.com>
2025-10-24 00:32:28 +0100

    .env file created and  added to gitignore

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBrLTtBb0evhg9OW95gGJXZcg-dOVP1iVA",
-  authDomain: "assignment-09-pet-care.firebaseapp.com",
-  projectId: "assignment-09-pet-care",
-  storageBucket: "assignment-09-pet-care.firebasestorage.app",
-  messagingSenderId: "753220443555",
-  appId: "1:753220443555:web:98be27561152ed2c990e16"
+ apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
  const app = initializeApp(firebaseConfig);
